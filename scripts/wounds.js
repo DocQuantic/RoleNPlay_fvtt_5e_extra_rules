9f342cd29e11923568d8f077df66db893c3db201
DocQuantic <w.magrini@yahoo.fr>
2024-01-26 21:08:08 +0100

    Create window display action

--- scripts/wounds.js
+++ scripts/wounds.js
@@ -1,3 +1,28 @@
+class WoundConfig extends FormApplication {
+  static get defaultOptions() {
+    const defaults = super.defaultOptions;
+  
+    const overrides = {
+      height: 'auto',
+      id: 'todo-list',
+      template: "modules/RoleNPlay_fvtt_5e_extra_rules/templates/wounds.hbs",
+      title: 'Blessure',
+      ability: 'str'
+    };
+  
+    const mergedOptions = foundry.utils.mergeObject(defaults, overrides);
+    
+    return mergedOptions;
+  }
+
+  getData() {
+    // Send data to the template
+    return {
+      color: 'red',
+    };
+  }
+}
+
 Hooks.on('renderActorSheet', (actorSheet5eCharacter, html, data) => {
   const actor = actorSheet5eCharacter.actor;
   if(actor.type=='character'){
