2f6518cfe48e79a02e3b2dbcf1b037275366b7c2
William Magrini <35266773+DocQuantic@users.noreply.github.com>
2024-01-29 06:22:28 +0100

    add flags to new actor

--- scripts/wounds.js
+++ scripts/wounds.js
@@ -56,13 +56,40 @@ Hooks.on('renderActorSheet', (actorSheet5eCharacter, html, data) => {
   }
 });
 
+
 Hooks.on('preCreateActor', (doc, data, options, userId) => {
   if (doc.type === 'character') {
-      doc.updateSource({'flags.midi-qol.disadvantage.ability.check.???': 0})
-      doc.updateSource({'flags.wounds5e.dex.isWounded': 0})
+    for (const key of Object.keys(abilityDict)) { 
+      let flagQOL = 'flags.midi-qol.disadvantage.ability.check.' + key;
+      let flagWounds = 'flags.wounds5e.' + key;
+
+      doc.updateSource({'flags.midi-qol.disadvantage' : {
+        'str.check.': 0,
+        'dex.check' : 0,
+        'con.check' : 0,
+        'int.check' : 0,
+        'wis.check' : 0,
+        'cha.check' : 0
+      }});
+      doc.updateSource({'flags.wounds5e': {
+        'str.isWounded': 0,
+        'str.daysToRest': 0,
+        'dex.isWounded': 0,
+        'dex.daysToRest': 0,
+        'con.isWounded': 0,
+        'con.daysToRest': 0,
+        'int.isWounded': 0,
+        'int.daysToRest': 0,
+        'wis.isWounded': 0,
+        'wis.daysToRest': 0,
+        'cha.isWounded': 0,
+        'cha.daysToRest': 0}
+      });
+    };
+    console.log(doc);
   }
-  console.log(doc)
 })
+
 
 Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
   registerPackageDebugFlag(ToDoList.ID);
