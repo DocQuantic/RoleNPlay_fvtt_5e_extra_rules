71a3c083d49b2267cb2b362c7a5ff62dc41ff841
DocQuantic <w.magrini@yahoo.fr>
2024-01-30 16:56:07 +0100

    Update data with window

--- scripts/wounds.js
+++ scripts/wounds.js
@@ -23,7 +23,9 @@ class WoundConfig extends FormApplication {
       height: 'auto',
       id: 'wound-conf',
       template: "modules/RoleNPlay_fvtt_5e_extra_rules/templates/wounds.hbs",
-      title: 'Configuration des blessures'
+      title: 'Configuration des blessures',
+      closeOnSubmit: true, // do not close when submitted
+      submitOnChange: true, // submit when any input changes
     };
   
     const mergedOptions = foundry.utils.mergeObject(defaults, overrides);
@@ -45,28 +47,42 @@ class WoundConfig extends FormApplication {
     
     await setProperty(this.actor, 'flags.wounds5e.' + this.ability + '.isWounded', expandedData["isWounded"]);
     await setProperty(this.actor, 'flags.wounds5e.' + this.ability + '.daysToHeal', expandedData["daysToHeal"]);
-
-    /*
-    if(getProperty(this.actor, 'flags.wounds5e.' + this.ability + '.daysToHeal') == 0){
-      setProperty(this.actor, 'flags.wounds5e.' + this.ability + '.isWounded', false);
-    }
-    */
-
-    if(getProperty(this.actor, 'flags.wounds5e.' + this.ability + '.isWounded')){
-      console.log("is wounded")
-      //this.actor.setFlag('midi-qol', 'disadvantage.' + this.ability + '.check', 1);
-    } else {
-      console.log("is not wounded")
-      //this.actor.setFlag('midi-qol', 'disadvantage.' + this.ability + '.check', 0);
-    }
     
-    console.log(this.actor);
+    if(event["type"]=='submit'){
+      
+      if(await getProperty(this.actor, 'flags.wounds5e.' + this.ability + '.daysToHeal') == 0){
+        await setProperty(this.actor, 'flags.wounds5e.' + this.ability + '.isWounded', false);
+      }
+      
+      if(await getProperty(this.actor, 'flags.wounds5e.' + this.ability + '.isWounded')){
+        await setProperty(this.actor, 'flags.midi-qol.disadvantage.ability.check.' + this.ability, 1);
+      } else {
+        await setProperty(this.actor, 'flags.midi-qol.disadvantage.ability.check.' + this.ability, 0);
+      }
+      
+      this.close();
+    }
+  }
 
-    this.render();
+  activateListeners(html) {
+    html.on('click', "[dialog-button]", this._handleButtonClick);
+  }
+
+  async _handleButtonClick(event) {
+    console.log('Button Clicked!');
+    /*
+    const clickedElement = $(event.currentTarget);
+    const action = clickedElement.data().action;
+    const toDoId = clickedElement.parents('[data-todo-id]')?.data()?.todoId;
+    
+    console.log('Button Clicked!');
+    */
   }
 }
 
-Hooks.on('renderActorSheet', (actor, html, data) => {
+Hooks.on('renderActorSheet', (actorSheet, html, data) => {
+  actor = actorSheet.actor
+  console.log(actor)
   if(actor.type=='character'){
     const abilityItem = html.find(`[class="ability "]`);
 
